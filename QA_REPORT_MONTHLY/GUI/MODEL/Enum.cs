4d49938f12a6ea4387e6efd8347c624c79aa5ad3
Sys009 <Sys009@tvntaishodom0.com>
2023-03-06 15:47:16 +0700

    Change thay đổi select định dạng

--- QA_REPORT_MONTHLY/GUI/MODEL/Enum.cs
+++ QA_REPORT_MONTHLY/GUI/MODEL/Enum.cs
@@ -8,7 +8,7 @@ namespace QA_REPORT_MONTHLY.MODEL
 {
     public class MdlComment
     {
-        public const string TYPE_FILE_SELECT = "Excel file: |*.xlsx;xlsm;xls";
+        public const string TYPE_FILE_SELECT = "Excel file: |*.xlsx;*xlsm;*xls";
 
         public const string CLICK_FILE_DATA = "CLICK_FILE_DATA";
         public const string CLICK_FILE_ERROR = "CLICK_FILE_ERROR";
